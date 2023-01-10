12ccf184c8a73c3913185072eca68e5de11909b8
Peter Kuehne <peter.kuehne@gmail.com>
2023-01-10 20:42:11 +0000

    Update keymap and fix nvim setup + ssh

--- private_dot_config/nvim/lua/keymaps.lua
+++ private_dot_config/nvim/lua/keymaps.lua
@@ -1,6 +1,8 @@
 local map = vim.api.nvim_set_keymap
 
-map('n', '<F9>',          ':NvimTreeToggle<CR>', {})
-map('n', '<F10>',         ':TagbarToggle<CR>', {})
-map('n', '<Leader>l',     ':IndentLinesToggle<CR>', {})
-map('n', '<Leader>ff',    ':Telescope find_files<CR>', {})
+map('n', '<F9>',            ':NvimTreeToggle<CR>', {})
+map('n', '<F10>',           ':TagbarToggle<CR>', {})
+map('n', '<Leader>l',       ':IndentLinesToggle<CR>', {})
+map('n', '<Leader>ff',      ':Telescope find_files<CR>', {})
+map('n', '<Leader>b',       ':Telescope buffers<CR>', {})
+map('n', '<Leader>g',       ':Git<CR>', {})
--- run_once_after_nvim_setup.sh
+++ run_once_after_nvim_setup.sh
@@ -1,18 +1,19 @@
 #! /bin/bash
 
-echo "-- Checking for nvim installation"
+echo "-- Checking for neovim installation"
 which nvim > /dev/null 2>&1
 if [[ $? -eq "1" ]]
 then
-  echo "?? nvim needs to be installed"
-  sudo apt update && sudo apt install nvim
+  echo "?? neovim needs to be installed"
+  sudo add-apt-repository ppa:neovim-ppa/stable
+  sudo apt update && sudo apt install -y neovim
 else
-  echo "-- nvim is installed"
+  echo "-- neovim is installed"
 fi
 
-echo "-- Installing nvim plugin manager"
+echo "-- Installing neovim plugin manager"
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
    "${HOME}/.local/share/nvim/site/pack/packer/start/packer.nvim"
 
-echo "-- Installing nvim plugins"
+echo "-- Installing neovim plugins"
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
