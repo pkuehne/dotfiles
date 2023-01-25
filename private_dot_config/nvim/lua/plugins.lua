4ca134b4f4afac8ce0c9712f1f12ddd588c71af9
Peter Kuehne <pkuehne@bloomberg.net>
2023-01-25 16:42:41 +0000

    Cleaner LSP config for neovim
    * Keymaps moved to separate function
    * Individual setups for rust and lua
    * Fix for wrong use of `local function`
    * Use of rust-tools

--- private_dot_config/nvim/lua/plugins.lua
+++ private_dot_config/nvim/lua/plugins.lua
@@ -3,8 +3,38 @@
 -- Only required if you have packer configured as `opt`
 -- vim.cmd [[packadd packer.nvim]]
 
-local function empty_config()
+function empty_config()
+end
 
+function lsp_on_attach(client, bufnr)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+    -- Mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local bufopts = { noremap = true, silent = true, buffer = bufnr }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+    vim.keymap.set('n', '<space>wl', function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, bufopts)
+    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+    vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+    vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+    vim.api.nvim_create_autocmd("BufWritePre", {
+        buffer = bufnr,
+        callback = function()
+            -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+            vim.lsp.buf.format({ bufnr = bufnr })
+        end,
+    })
 end
 
 return require('packer').startup(function(use)
@@ -87,6 +117,11 @@ return require('packer').startup(function(use)
         'tpope/vim-obsession',
         config = empty_config,
     }
+    use { -- Rust LSP support
+        'simrat39/rust-tools.nvim',
+        config = function()
+        end
+    }
     use { -- LSP install manager
         "williamboman/mason.nvim",
         config = empty_config,
@@ -98,48 +133,43 @@ return require('packer').startup(function(use)
     use { -- LSP Configuration and setup
         "neovim/nvim-lspconfig",
         config = function()
-            require("mason").setup()
+            require("mason").setup({
+                ui = {
+                    icons = {
+                        package_installed = "✓",
+                        package_pending = "➜",
+                        package_uninstalled = "✗",
+                    },
+                },
+            })
             require("mason-lspconfig").setup()
             require("mason-lspconfig").setup_handlers {
                 -- default handler.
                 function(server_name)
                     require("lspconfig")[server_name].setup {
-                        on_attach = function(client, bufnr)
-                            -- Enable completion triggered by <c-x><c-o>
-                            vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-                            -- Mappings.
-                            -- See `:help vim.lsp.*` for documentation on any of the below functions
-                            local bufopts = { noremap = true, silent = true, buffer = bufnr }
-                            vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-                            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-                            vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-                            vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-                            vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-                            vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-                            vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-                            vim.keymap.set('n', '<space>wl', function()
-                                print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-                            end, bufopts)
-                            vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-                            vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-                            vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-                            vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-                            vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
-                            vim.api.nvim_create_autocmd("BufWritePre", {
-                                buffer = bufnr,
-                                callback = function()
-                                    -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-                                    vim.lsp.buf.format({ bufnr = bufnr })
-                                end,
-                            })
-                        end
+                        on_attach = lsp_on_attach,
                     }
                 end,
                 -- Dedicated handlers
-                -- ["rust_analyzer"] = function ()
-                --     require("rust-tools").setup {}
-                -- end
+                ["rust_analyzer"] = function()
+                    require("rust-tools").setup ({
+                        server = {
+                            on_attach = lsp_on_attach
+                        }
+                    })
+                end,
+                ["sumneko_lua"] = function()
+                    require("lspconfig")["sumneko_lua"].setup {
+                        on_attach = lsp_on_attach,
+                        settings = {
+                            LUA = {
+                                diagnostics = {
+                                    globals = { "vim" }
+                                }
+                            }
+                        }
+                    }
+                end,
             }
         end
     }
