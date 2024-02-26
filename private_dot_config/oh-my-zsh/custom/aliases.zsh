2e42ee503a311f8cd208743a2d42f8d7d2c74367
Peter Kuehne <pkuehne@bloomberg.net>
2024-02-26 12:26:38 +0000

    Add tmux alias to zsh

--- private_dot_config/oh-my-zsh/custom/aliases.zsh
+++ private_dot_config/oh-my-zsh/custom/aliases.zsh
@@ -2,4 +2,8 @@
 # This file is managed by chezmoi, don't edit it directly !!!
 #############################################################
 
+# Use neovim instead of vim
 alias vim="nvim"
+
+# Always enable Unicode and 256 colours
+alias tmux="tmux -u -2"
