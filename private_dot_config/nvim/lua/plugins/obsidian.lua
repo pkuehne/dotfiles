eb748830e7f7c7758c206fb1bf75b00fae754974
Peter Kuehne <pkuehne@bloomberg.net>
2024-03-06 13:06:54 +0000

    Add Obsidian

---
+++ private_dot_config/nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,45 @@
+return {
+  "epwalsh/obsidian.nvim",
+  version = "*", -- recommended, use latest release instead of latest commit
+  lazy = true,
+  ft = "markdown",
+  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+  -- event = {
+  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+  --   "BufReadPre path/to/my-vault/**.md",
+  --   "BufNewFile path/to/my-vault/**.md",
+  -- },
+  dependencies = {
+    -- Required.
+    "nvim-lua/plenary.nvim",
+    "hrsh7th/nvim-cmp",
+    "nvim-telescope/telescope.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  opts = {
+    workspaces = {
+      {
+        name = "work",
+        path = "~/Obsidian/Work/",
+      },
+    },
+    daily_notes = {
+      -- Optional, if you keep daily notes in a separate directory.
+      folder = "Journal",
+      -- Optional, if you want to change the date format for the ID of daily notes.
+      date_format = "%Y/%m/%Y-%m-%d-%A",
+      -- Optional, if you want to change the date format of the default alias of daily notes.
+      alias_format = "%B %-d, %Y",
+      -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+      template = "Templates/Daily Template"
+    },
+    completion = {
+      nvim_cmp = true,
+      min_chars = 2,
+    },
+    picker = {
+      name = "telescope.nvim",
+    },
+  },
+}
