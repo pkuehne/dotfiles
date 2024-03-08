bb47f651015b103db04d6a539760fa57e90053f5
Peter Kuehne <pkuehne@bloomberg.net>
2024-03-08 12:03:17 +0000

    Revamp external tool/file inclusion

--- private_dot_config/oh-my-zsh/custom/aliases.zsh
+++ private_dot_config/oh-my-zsh/custom/aliases.zsh
@@ -7,5 +7,22 @@ if type nvim &> /dev/null; then
   alias vim='nvim'
 fi
 
+# Use bat instead of cat
+if type bat &> /dev/null; then
+  alias cat='bat'
+fi
+
+if type exa &> /dev/null; then
+  alias ls='exa --grid --time-style=long-iso --icons'
+  alias ll='ls --long'
+  alias ll='ls --long --all'
+  alias lg='ls --long --git'
+  alias tree='exa --tree'
+fi
+
+if type rg &> /dev/null; then
+  alias grep='rg'
+fi
+
 # Always enable Unicode and 256 colours
 alias tmux="tmux -u -2"
--- private_dot_config/oh-my-zsh/custom/path.zsh
+++ private_dot_config/oh-my-zsh/custom/path.zsh
@@ -2,7 +2,8 @@
 # This file is managed by chezmoi, don't edit it directly !!!
 #############################################################
 
-path+=("${HOME}/bin/")
+path+=("${HOME}/bin")
+path+=("${HOME}/.local/bin")
 if [[ -d "${HOME}/.cargo/bin" ]]; then
   path+=("${HOME}/.cargo/bin")
 fi
