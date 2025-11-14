5ee0ff71a7b44a07c0cf3e4c82747bda8a15efd4
Peter Kuehne <peter.kuehne@gmail.com>
2025-11-14 19:27:20 +0000

    Fix up wayland config for wsl

--- modules/common.nix
+++ modules/common.nix
@@ -5,9 +5,27 @@
 
   home = {
     packages = with pkgs;
-      [ direnv dust lazygit python3 ripgrep zsh-powerlevel10k ]
-      ++ [ agenix.packages.${pkgs.system}.default ];
+      [
+        direnv
+        dust
+        lazygit
+        python3
+        ripgrep
+        zsh-powerlevel10k
+        wl-clipboard
+        statix
+        alejandra
+        nixd
+        nil
+      ] ++ [ agenix.packages.${pkgs.system}.default ];
     stateVersion = "24.05"; # do not bump blindly
+    sessionVariables = {
+      WAYLAND_DISPLAY = "wayland-0";
+      XDG_RUNTIME_DIR = if builtins.pathExists "/mnt/wslg/runtime-dir" then
+        "/mnt/wslg/runtime-dir"
+      else
+        "/run/user/1000";
+    };
   };
 
   programs = {
